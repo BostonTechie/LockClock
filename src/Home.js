767eb6d32a97cffdce1d6eb64f72d68d89d7ee53
Andrew D Urquhart <85643280+BostonTechie@users.noreply.github.com>
2022-03-26 11:37:35 -0400

    working

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Home = () => {
     
 const baseURL = 'http://localhost:8000'
+const [userView, setUserView] = useState('')
 
+const retrieveUser = async () => {
+  const url = `${baseURL}/get`
+  const userData = await axios.get(url)
+  setUserView(userData.data)
 
-fetch(`${baseURL}/get`)
-    .then(res => res.json())
-    .then(data =>console.log(data))
+}
     
+useEffect(() => {
+  retrieveUser()
+}, [])
 
-  return (
-    <div>Home</div>
-  )
+
+
+
+
+return (
+    
+  <div>hello</div>
+)
 }
 
 export default Home
