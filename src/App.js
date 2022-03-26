f0d0977c523f11181ac094793dadc42513a7134f
Andrew D Urquhart <85643280+BostonTechie@users.noreply.github.com>
2022-03-26 12:10:08 -0400

    got a map to work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from "./Home";
+import Home from "./views/Home";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Home/>} />
-        </Routes>
+      </Routes>
     </div>
    
   );
