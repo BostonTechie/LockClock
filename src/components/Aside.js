1d3def8bf6e3a6fb6b175f71bcc9fea324037df3
Andrew D Urquhart <85643280+BostonTechie@users.noreply.github.com>
2022-03-31 21:49:26 -0400

    re deploy

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,51 +1,53 @@
 import React from 'react'
 import '../styled/css/aside.css'
 import Box from '@mui/material/Box';
-import { createSvgIcon } from '@mui/material/utils';
-import Nav from "react-bootstrap/Nav"
-import Navbar from "react-bootstrap/Navbar";
-import { LinkContainer } from "react-router-bootstrap"
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import AppBar from '@mui/material/AppBar';
+import { flexbox } from '@mui/system';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import MenuIcon from '@mui/icons-material/Menu';
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import { styled } from '@mui/material/styles';
 
-const ClockIcon = createSvgIcon(
-  <path d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22A9,9 0 0,0 21,13A9,9 0 0,0 12,4M12.5,8H11V14L15.75,16.85L16.5,15.62L12.5,13.25V8M7.88,3.39L6.6,1.86L2,5.71L3.29,7.24L7.88,3.39M22,5.72L17.4,1.86L16.11,3.39L20.71,7.25L22,5.72Z"  />,
-  'Alarm',
-);
+
+const StyledFab = styled(Fab)({
+  position: 'absolute',
+  zIndex: 1,
+  top: -30,
+  left: 0,
+  right: 0,
+  margin: '0 auto',
+});
 
 const Aside = () => {
 
-  function CreateSvgIcon() {
-    return (
-      <Box
-        sx={{
-          '& > :not(style)': {
-            m: 2,
-          },
-        }}
-      >
-        <ClockIcon />
-        <ClockIcon color="primary" />
-      </Box>
-    );
-  }
+  
 
   return (
-    <div>
-    <aside >
-      <Navbar>
-      <Navbar.Collapse className="justify-content-end">
-        <Nav activeKey={window.location.pathname}>
-          <LinkContainer to="/timesheet">
-            <Nav.Link><ClockIcon></ClockIcon></Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/">
-            <Nav.Link></Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-      </Navbar>
-        
-    </aside>
-    </div>
+    <React.Fragment>
+    <AppBar position="fixed" color="primary" sx={{ display: 'inline-flex', flexDirection: 'column' , top: 'auto', bottom: 0 }}>
+    <Toolbar>
+      <IconButton color="inherit" aria-label="open drawer">
+        <MenuIcon />
+      </IconButton>
+      <StyledFab color="secondary" aria-label="add">
+        <AddIcon />
+      </StyledFab>
+      <Box sx={{ flexGrow: 1 }} />
+      <IconButton color="inherit">
+        <SearchIcon />
+      </IconButton>
+      <IconButton color="inherit">
+        <MoreIcon />
+      </IconButton>
+    </Toolbar>
+  </AppBar>
+</React.Fragment>
+
   )
 }
 
